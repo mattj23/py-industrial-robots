fd3b89badb42622147231898e7b1cf3984a0e53c
Matt Jarvis <mattj23@gmail.com>
2025-03-29 23:38:35 -0400

    experimenting with micro meshes

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,25 @@
-mod utility;
-
-use industrial_robots::{try_convert, Isometry3};
 use pyo3::prelude::*;
-use pyo3::types::PyList;
-use utility::{fanuc_with_joints, from_mat4, to_mat4};
 
-#[pyfunction]
-fn fanuc_fk(joints: Vec<f64>) -> PyResult<Vec<Vec<f64>>> {
-    let robot = fanuc_with_joints(joints);
-    let mut poses = Vec::new();
-    for pose in robot.link_poses() {
-        poses.push(from_mat4(pose.to_matrix()));
-    }
-    Ok(poses)
+mod utility;
+mod conversions;
+mod fanuc;
+mod mesh;
+
+fn register_fanuc_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
+    let child = PyModule::new(parent_module.py(), "_fanuc")?;
+    child.add_class::<fanuc::Crx>()?;
+
+    parent_module.add_submodule(&child)
 }
 
-#[pyfunction]
-fn fanuc_ik(target_pose: Vec<f64>, starting_joints: Vec<f64>) -> PyResult<Vec<f64>> {
-    let robot = fanuc_with_joints(starting_joints);
+#[pymodule(name = "industrial_robots")]
+fn py_industrial_robots(m: &Bound<'_, PyModule>) -> PyResult<()> {
 
-    let pose = to_mat4(target_pose);
-    let target: Isometry3<f64> = try_convert(pose).unwrap();
+    register_fanuc_module(m)?;
 
-    if let Some(joints) = robot.find_joints(&target) {
-        let py_list = joints.iter().map(|x| x.to_owned()).collect::<Vec<f64>>();
-        Ok(py_list)
-    } else {
-        Err(pyo3::exceptions::PyValueError::new_err("No solution found"))
-    }
-}
+    m.add_class::<utility::Frame3>()?;
+    m.add_function(wrap_pyfunction!(mesh::micro_serialize, m)?)?;
+    m.add_function(wrap_pyfunction!(mesh::micro_deserialize, m)?)?;
 
-#[pymodule]
-fn py_industrial_robots(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(fanuc_fk, m)?)?;
-    m.add_function(wrap_pyfunction!(fanuc_ik, m)?)?;
     Ok(())
 }
--- src/utility.rs
+++ src/utility.rs
@@ -1,58 +1,159 @@
-use industrial_robots::robot::FanucLrMate200id;
-use industrial_robots::{try_convert, Isometry3, Matrix4, Vector3};
+use industrial_robots::nalgebra::{try_convert, Matrix4};
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn, PyUntypedArrayMethods};
+use numpy::ndarray::ArrayD;
+use pyo3::exceptions::PyValueError;
+use pyo3::IntoPyObjectExt;
+use pyo3::prelude::*;
+use crate::conversions::{array_to_points3, array_to_vectors3};
 
-pub fn fanuc_with_joints(joints: Vec<f64>) -> FanucLrMate200id {
-    // TODO: Decide if we should verify if there are at least six joints
-    let mut robot = FanucLrMate200id::new();
-    robot.set_joints(&joints);
-    robot
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct Frame3 {
+    inner: industrial_robots::Frame3,
 }
 
-pub fn to_mat4(py_list: Vec<f64>) -> Matrix4<f64> {
-    let mut mat = Matrix4::zeros();
-    for i in 0..4 {
-        for j in 0..4 {
-            mat[(i, j)] = py_list[i * 4 + j];
+impl Frame3 {
+    pub fn from_inner(inner: industrial_robots::Frame3) -> Self {
+        Self { inner }
+    }
+
+    pub fn get_inner(&self) -> &industrial_robots::Frame3 {
+        &self.inner
+    }
+}
+
+#[pymethods]
+impl Frame3 {
+    fn __repr__(&self) -> String {
+        format!(
+            "<Frame3 t=[{}, {}, {}] r=[{}, {}, {}, {}]>",
+            self.inner.translation.x,
+            self.inner.translation.y,
+            self.inner.translation.z,
+            self.inner.rotation.i,
+            self.inner.rotation.j,
+            self.inner.rotation.k,
+            self.inner.rotation.w,
+        )
+    }
+
+    #[new]
+    fn new(matrix: PyReadonlyArrayDyn<'_, f64>) -> PyResult<Self> {
+        if matrix.shape().len() != 2 || matrix.shape()[0] != 4 || matrix.shape()[1] != 4 {
+            return Err(PyValueError::new_err("Expected 4x4 matrix"));
+        }
+
+        let mut array = [0.0; 16];
+        for (i, value) in matrix.as_array().iter().enumerate() {
+            array[i] = *value;
+        }
+
+        let inner = try_convert(Matrix4::from_row_slice(&array))
+            .ok_or(PyValueError::new_err("Could not convert to isometry"))?;
+
+        Ok(Self { inner })
+    }
+
+    #[staticmethod]
+    fn from_translation(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            inner: industrial_robots::Frame3::translation(x, y, z),
         }
     }
-    mat
-}
 
-pub fn from_mat4(mat: Matrix4<f64>) -> Vec<f64> {
-    let mut py_list = Vec::new();
-    for i in 0..4 {
-        for j in 0..4 {
-            py_list.push(mat[(i, j)]);
+    #[staticmethod]
+    fn from_rotation(angle: f64, a: f64, b: f64, c: f64) -> Self {
+        let axis = industrial_robots::UnitVector3::new_normalize(industrial_robots::Vector3::new(a, b, c));
+        let rot_vec = axis.into_inner() * angle;
+
+        Self {
+            inner: industrial_robots::Frame3::rotation(rot_vec),
         }
     }
-    py_list
+
+    fn inverse(&self) -> Self {
+        Self {
+            inner: self.inner.inverse(),
+        }
+    }
+
+    fn __matmul__(
+        &self,
+        other: &Frame3,
+    ) -> PyResult<Self> {
+        Ok(Frame3::from_inner(self.inner * other.inner))
+    }
+
+    fn as_numpy<'py>(&self, py: Python<'py>) -> Bound<'py, PyArrayDyn<f64>> {
+        let mut result = ArrayD::zeros(vec![4, 4]);
+        let m = self.inner.to_matrix();
+        // TODO: In a rush, fix this later
+        result[[0, 0]] = m.m11;
+        result[[0, 1]] = m.m12;
+        result[[0, 2]] = m.m13;
+        result[[0, 3]] = m.m14;
+        result[[1, 0]] = m.m21;
+        result[[1, 1]] = m.m22;
+        result[[1, 2]] = m.m23;
+        result[[1, 3]] = m.m24;
+        result[[2, 0]] = m.m31;
+        result[[2, 1]] = m.m32;
+        result[[2, 2]] = m.m33;
+        result[[2, 3]] = m.m34;
+        result[[3, 0]] = m.m41;
+        result[[3, 1]] = m.m42;
+        result[[3, 2]] = m.m43;
+        result[[3, 3]] = m.m44;
+        result.into_pyarray(py)
+    }
+
+    #[staticmethod]
+    fn identity() -> Self {
+        Self {
+            inner: industrial_robots::Frame3::identity(),
+        }
+    }
+
+    fn transform_points<'py>(
+        &self,
+        py: Python<'py>,
+        points: PyReadonlyArrayDyn<'py, f64>,
+    ) -> PyResult<Bound<'py, PyArrayDyn<f64>>> {
+        let points = array_to_points3(&points.as_array())?;
+        let mut result = ArrayD::zeros(vec![points.len(), 3]);
+
+        for (i, point) in points.iter().enumerate() {
+            let transformed = self.inner * point;
+            result[[i, 0]] = transformed.x;
+            result[[i, 1]] = transformed.y;
+            result[[i, 2]] = transformed.z;
+        }
+
+        Ok(result.into_pyarray(py))
+    }
+
+    fn transform_vectors<'py>(
+        &self,
+        py: Python<'py>,
+        vectors: PyReadonlyArrayDyn<'py, f64>,
+    ) -> PyResult<Bound<'py, PyArrayDyn<f64>>> {
+        let vectors = array_to_vectors3(&vectors.as_array())?;
+        let mut result = ArrayD::zeros(vec![vectors.len(), 3]);
+
+        for (i, vector) in vectors.iter().enumerate() {
+            let transformed = self.inner * vector;
+            result[[i, 0]] = transformed.x;
+            result[[i, 1]] = transformed.y;
+            result[[i, 2]] = transformed.z;
+        }
+
+        Ok(result.into_pyarray(py))
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn mat4_orders_correctly() {
-        let test_list = vec![
-            1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
-        ];
-        let mat = to_mat4(test_list);
-        assert_eq!(mat[(0, 0)], 1.0);
-        assert_eq!(mat[(0, 1)], 2.0);
-        assert_eq!(mat[(0, 2)], 3.0);
-        assert_eq!(mat[(0, 3)], 4.0);
-        assert_eq!(mat[(1, 0)], 5.0);
-        assert_eq!(mat[(1, 1)], 6.0);
-        assert_eq!(mat[(1, 2)], 7.0);
-        assert_eq!(mat[(1, 3)], 8.0);
-        assert_eq!(mat[(2, 0)], 9.0);
-        assert_eq!(mat[(2, 1)], 10.0);
-        assert_eq!(mat[(2, 2)], 11.0);
-        assert_eq!(mat[(2, 3)], 12.0);
-        assert_eq!(mat[(3, 0)], 13.0);
-        assert_eq!(mat[(3, 1)], 14.0);
-        assert_eq!(mat[(3, 2)], 15.0);
-        assert_eq!(mat[(3, 3)], 16.0);
-    }
 }
